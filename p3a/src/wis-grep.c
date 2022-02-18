125ef4028e1d35257efa2d24a077383567a38885
John Schiltz <johnschiltz2000@gmail.com>
2022-02-18 03:52:52 -0600

    Finish wis-grep

---
+++ p3a/src/wis-grep.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "fileutil.h"
+
+int main(int argc, char *argv[])
+{
+
+  int i;
+  switch (argc)
+  {
+    case 1: // Return syntax for calling wis-grep
+      printf("wis-grep: searchterm [file ...]\n");
+      break;
+    case 2: // search through standard in
+    {
+      fileOption *fo = initFileOption();
+      fo->fileName = "stdio";
+      fo->fp = stdin;
+      searchFile(fo, argv[1]);
+      free(fo);
+    }break;
+    default: // Search through all the files
+    {
+      printf("Mode = Default: search through all files\n");
+      printf("Search term: %s\n", argv[1]);
+      for (i = 2; i < argc; i++)
+      {
+        fileOption *fo = initFileOption();
+        getFile(fo,argv[i]);
+        searchFile(fo, argv[1]);
+        closeFile(fo);
+      }
+    }
+  }
+  exit(1);
+}
