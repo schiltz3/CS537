2c9318b1d0a28cb88a8516f544a8d131d472a8e9
John Schiltz <johnschiltz2000@gmail.com>
2022-03-08 00:12:48 -0600

    Move old

--- p4/old/shellUtil.old.h
+++ p4/old/shellUtil.old.h
@@ -20,33 +20,6 @@
 #define RED "\x1B[0;31m"
 #define NC "\x1B[0m"
 
-enum cmd_t{
-  NONE,
-  EXEC,
-  REDIR,
-  LIST
-};
-
-struct exec_cmd{
-  enum cmd_t type;
-  char *argv;
-};
-
-struct redir_cmd{
-  enum cmd_t type;
-  FILE *fp;
-};
-
-struct list_cmd{
-  enum cmd_t type;
-};
-
-union cmd{
-  struct exec_cmd *exec;
-  struct redir_cmd *redir;
-  struct list_cmd *list;
-};
-
 typedef struct Lines
 {
   char **lines;
