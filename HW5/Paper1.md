90abc0259d7c210cbb98a1f00bde559f5b7bd958
John Schiltz <johnschiltz2000@gmail.com>
2022-04-27 23:46:56 -0500

    Finished HW5

---
+++ HW5/Paper1.md
@@ -0,0 +1,25 @@
+# Network-Attached Storage: Data Storage Applications
+
+### What is the primary problem that the author hopes to address?
+
+The author hopes to address the problems of multi-to-one and one-to-multi configurations, the slow speed of current file servers, simplify the management and distribution of the file server, centralize storage of data, and make the data easily available.
+
+### What are the authors' main contribution to the solution of this problem?
+
+The author contributed the NAS system which is a file-level storage server connected to a network. Hence Network Attached Storage.
+
+### What is the most important limitation of their solution?
+
+Network stability is a limitation of NAS
+
+### According to the authors, the previous work most closely related to theirs is? but that work didn't solve the problem entirely because?
+
+3Com and Auspex Systems developed MAS and NFS servers, but they did not important the contributions listed above
+
+### This system was evaluated by?
+
+There was no evaluation.
+
+### One other possible way to evaluate the work would be?
+
+A study of several NAS users where the users used a NAS for several weeks, periodically filling out System Usability Score(SUS) survey could have provided valuable feedback.
--- HW5/main_q1_1.c
+++ HW5/main_q1_1.c
@@ -9,7 +9,6 @@
 struct thread_arg_s
 {
   int *counter;
-  pthread_mutex_t *count_lock;
   int thread_limit;
   int thread_num;
 };
@@ -17,7 +16,6 @@ struct thread_arg_s
 /*-Global Variables------------------------------------------*/
 // Create a global counter
 int counter = 0;
-pthread_mutex_t count_lock;
 
 /*-Function Prototypes---------------------------------------*/
 void *increment_counter(void *vargp);
@@ -29,14 +27,12 @@ int main()
   // Source 1: https://www.geeksforgeeks.org/multithreading-c-2/
   // Source 2: man pthread_create
   // Source 3: man pthread_join
-  // Source 4: https://www.geeksforgeeks.org/mutex-lock-for-linux-thread-synchronization/
 
   pthread_t thread_id[thread_count];
   for (int thread_index = 0; thread_index < thread_count; thread_index++)
   {
     thread_args[thread_index].counter = &counter;
-    thread_args[thread_index].count_lock = &count_lock;
-    thread_args[thread_index].thread_limit = 10000 / thread_count;
+    thread_args[thread_index].thread_limit = 100000 / thread_count;
     thread_args[thread_index].thread_num = thread_index;
   }
 
@@ -58,15 +54,11 @@ int main()
 void *increment_counter(void *vargp)
 {
   struct thread_arg_s args = *((struct thread_arg_s *)vargp);
-  pthread_mutex_lock(args.count_lock);
-
   // printf("Thread ID: %d\tCurrent Count: %d\n", args.thread_num, *args.counter);
 
   for (int i = 0; i < args.thread_limit; i++)
   {
     ++*args.counter;
   }
-
-  pthread_mutex_unlock(args.count_lock);
   return NULL;
 }
